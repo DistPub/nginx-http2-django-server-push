1bc1b6002b6538316f8b6d340723afae2353676c
Smite Chow <xiaopengyou2no1@gmail.com>
2018-02-27 15:50:04 +0800

    dynamic add link

--- applications/demo/middlewares.py
+++ applications/demo/middlewares.py
@@ -1,3 +1,4 @@
+import re
 from django.conf import settings
 
 
@@ -6,5 +7,19 @@ class AddLinkHeaderMiddleware(object):
         if not settings.ENABLE_SERVER_PUSH:
             return response
 
-        response['Link'] = '</static/styles/index.css>; as=style; rel=preload'
+        if 'text/html' not in response['content-type']:
+            return response
+
+        content = str(response.content)
+        links = re.findall(r'(<(link|script|img).+?/?>)', content)
+        link_headers = []
+        for link in links:
+            if re.search(r'rel=.+preload.+', link[0]) and re.search(r'as=.+\w.+', link[0]):
+                if link[1] != 'link':
+                    href = re.search(r'src=[\'"](.+?)[\'"]', link[0]).group(1)
+                else:
+                    href = re.search(r'href=[\'"](.+?)[\'"]', link[0]).group(1)
+                as_value = re.search(r'as=[\'"](.+?)[\'"]', link[0]).group(1)
+                link_headers.append('<{}>; as={}; rel=preload'.format(href, as_value))
+        response['Link'] = ','.join(link_headers)
         return response
